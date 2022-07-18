adb33e0fb833e684d0546cb25462a1ff39d175b1
q s <1258920697@qq.com>
2022-07-18 21:06:54 +0800

    sentinel使用学习

--- nacos_learn_02/feign-demo/src/main/java/com/example/feign/FeignDemoApplication.java
+++ nacos_learn_02/feign-demo/src/main/java/com/example/feign/FeignDemoApplication.java
@@ -4,6 +4,7 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
