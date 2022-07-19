8a2fdeb29b13aeccc6f72dd0b7f53c6ebb1e4a0d
NewOrForever <bruce1203@126.com>
2022-07-19 22:05:02 +0800

    sentinel使用学习

--- sentinel_learn_01/sentinel-demo/src/main/java/com/example/sentinel/controller/HelloController.java
+++ sentinel_learn_01/sentinel-demo/src/main/java/com/example/sentinel/controller/HelloController.java
@@ -5,10 +5,17 @@ import com.alibaba.csp.sentinel.Sph;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.Tracer;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ClassName:HelloController
@@ -32,21 +39,41 @@ public class HelloController {
             entry = SphU.entry(RESOURCE_NAME);
             // 被保护的业务逻辑 - 实际就是给业务逻辑做个before的切面
             String str = "hello world";
-            logger.info(String.format("----------------> %s", str));
+            logger.info("----------------> {}", str);
 
             return str;
         } catch (BlockException e) {
-            e.printStackTrace();
-        } catch (Exception ex){
+            // 资源访问阻止，被限流或被降级  Sentinel定义异常  流控规则，降级规则，热点参数规则。。。。  服务降级(降级规则)
+            // 进行相应的处理操作
+            logger.info("block!");
+            return "被流控啦！";
+        } catch (Exception ex) {
+            // 若需要配置降级规则，需要通过这种方式记录业务异常  RuntimeException  服务降级  mock  feign:fallback
             Tracer.traceEntry(ex, entry);
-        }
-        finally {
+        } finally {
             if (entry != null) {
-                entry.close();
+                entry.exit();
             }
         }
 
         return null;
+    }
+
+    // 设置流控规则
+    @PostConstruct
+    private void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule flowRule = new FlowRule();
+        // 设置受保护的资源
+        flowRule.setResource(RESOURCE_NAME);
+        // 设置流控规则 QPS
+        flowRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // 设置受保护资源的阈值
+        flowRule.setCount(1);
+
+        rules.add(flowRule);
+        // 加载配置好的规则
+        FlowRuleManager.loadRules(rules);
     }
 
 }
